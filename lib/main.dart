6774cee5a10ed42674e92095247a8969d1d18bce
ChakaliMadhu6302769895 <Gauthamroyalz12324@gmail.com>
2023-08-23 11:05:39 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text("HELLO MADHU", textDirection: TextDirection.ltr , style: TextStyle(fontSize: 50 ,color: Color.fromARGB(400, 855, 0, 900) , fontStyle: FontStyle.italic)),
+    ),
+  );
+}
